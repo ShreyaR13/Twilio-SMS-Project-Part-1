49cfff7aaf6a7cf675e1b7bfbe395285c0f814f3
Shreya Rajwade <s4.shre@gmail.com>
2018-12-04 19:04:17 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
     // Get the PHP helper library from twilio.com/docs/php/install
     require_once 'twilio-php-master/Twilio/autoload.php'; // Loads the library
     use Twilio\Rest\Client;
-    $account_sid = 'AC6f189af575538e547c0c8bc566ecebba';
-    $auth_token = '73e55146dfda04d73a2e28deba19a417';
+    $account_sid = '';
+    $auth_token = '';
     $client = new Client($account_sid, $auth_token);
-    $messages = $client->messages->create('+19173490168', array(
-         'From' => '+12017201388',
+    $messages = $client->messages->create('', array(
+         'From' => '',
          'Body' => 'Hello from Shreya Rajwade CS 643 Fall 2017'
     ));
 ?>
